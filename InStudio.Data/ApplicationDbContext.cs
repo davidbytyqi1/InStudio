923433715658f898d5d1fa3a1cfe0f7b49b6f63a
David Bytyqi <dbytyqi.ext>
2024-09-30 19:07:07 +0200

    Identity User Added

---
+++ InStudio.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using InStudio.Data.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InStudio.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new IdentityConfig());
+        }
+    }
+}
---
+++ InStudio/Program.cs
@@ -0,0 +1,42 @@
+using InStudio.Data;
+using InStudio.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddCookie(IdentityConstants.ApplicationScheme)
+    .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services.AddIdentityCore<User>().
+    AddEntityFrameworkStores<ApplicationDbContext>().
+    AddApiEndpoints();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+ options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.ApplyMigrations();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapIdentityApi<User>();
+
+app.Run();
